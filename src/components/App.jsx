8ec5a05c47f64e633b2c2c703c7f9b72403bf103
Sergey Donyk <doncarlos@i.ua>
2023-04-23 14:07:59 +0300

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Form from './phoneBoock/Forma';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Form onSubmit={this.formSubmitHandler} />
+      </div>
+    );
+  }
+}
---
+++ src/components/phoneBoock/Forma.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = { contacts: [], name: '' };
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+  reset = () => this.setState({ contacts: [], name: '' });
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <button type="submit"> Add Contact</button>
+      </form>
+    );
+  }
+}
+export default Form;
